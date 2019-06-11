9350a1ac993c6d857ced45cbbb4cd2e16436ea12
gccpacman <gccpacman@gmail.com>
2019-06-11 11:11:49 +0800

    vuejs template example

--- journal/views.py
+++ journal/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     content = {
-        'days': [1, 2, 3]
+        'days': [1, 2, 3, 4]
     }
     return render(request, 'days.html', content)
