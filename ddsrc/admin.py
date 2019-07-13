0e28154d64c0527d0ddb85421cf8d8c99ab6c77d
gccpacman <gccpacman@gmail.com>
2019-07-13 10:35:24 +0800

    models admin support

--- ddsrc/admin.py
+++ ddsrc/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Road, Architecture, ArchitecturePicture
+
+admin.site.register(Road)
+admin.site.register(Architecture)
+admin.site.register(ArchitecturePicture)
