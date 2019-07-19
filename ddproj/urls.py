39f053d3273b126fc4f079475e69c16690dfcae0
gccpacman <gccpacman@gmail.com>
2019-07-19 14:31:47 +0800

    django restframework quickstart

--- ddproj/urls.py
+++ ddproj/urls.py
@@ -21,8 +21,6 @@ from django.views.generic import TemplateView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
 
-from main.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
--- ddsrc/urls.py
+++ ddsrc/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls import url, include
+from ddsrc.models import Road
+from rest_framework import routers, serializers, viewsets
+from rest_framework.response import Response
 
-from main import views
+class RoadSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Road
+        fields = ['name_chs', 'name_en', 'des', 'des2', 'lib_uri', 'temporal_value', \
+            'name_after', 'history_of_name', 'history_of_lib_uri', 'place_name', 'place_uri', \
+             'is_from_lib', 'lib_update_time', 'update_time', 'create_time',]
+
+class RoadViewSet(viewsets.ModelViewSet):
+    queryset = Road.objects.all()
+    serializer_class = RoadSerializer
+
+router = routers.DefaultRouter()
+router.register(r'roads', RoadViewSet)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
 ]
