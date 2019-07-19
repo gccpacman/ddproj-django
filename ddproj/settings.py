c5aa90964c83a75515f57fba2621c155b485530f
gccpacman <gccpacman@gmail.com>
2019-07-19 18:35:44 +0800

    disable restframework

--- ddproj/settings.py
+++ ddproj/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    # 'rest_framework',
     'ddsrc'
 ]
 
@@ -60,10 +60,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ddproj.urls'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
 
 TEMPLATES = [
     {
--- ddsrc/urls.py
+++ ddsrc/urls.py
@@ -1,27 +1,7 @@
 from django.conf.urls import url, include
 from django.urls import path
-from rest_framework import routers, serializers, generics
-from rest_framework.response import Response
 
 from ddsrc.models import Road
 
-
-class RoadSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Road
-        fields = ['name_chs', 'name_en', 'des', 'des2', 'lib_uri', 'temporal_value', \
-            'name_after', 'history_of_name', 'history_of_lib_uri', 'place_name', 'place_uri', \
-             'is_from_lib', 'lib_update_time', 'update_time', 'create_time',]
-
-class RoadListView(generics.ListAPIView):
-    queryset = Road.objects.all()
-    serializer_class = RoadSerializer
-
-class RoadDetailView(generics.RetrieveAPIView):
-    queryset = Road.objects.all()
-    serializer_class = RoadSerializer
-
 urlpatterns = [
-    url(r'^roads/$', RoadListView.as_view()),
-    url(r'^roads/<int:pk>/$', RoadDetailView.as_view()),
 ]
