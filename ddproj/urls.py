3ad26d6d09894fcba716cd750f9c94ea25231584
gccpacman <gccpacman@gmail.com>
2019-08-23 13:58:55 +0800

    update admin url

--- ddproj/urls.py
+++ ddproj/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('onroads/sogood/letsgo/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include("ddsrc.urls")),
     url(r'^media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
