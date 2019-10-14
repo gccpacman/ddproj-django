e33bc013882fe881a9e7ff0080490a661758097d
gccpacman <gccpacman@gmail.com>
2019-10-14 05:04:22 +0000

    remove template vue code

--- vuemain/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def index(request):
-    content = {
-        'days': [1, 2, 3, 4]
-    }
-    return render(request, 'index.html', content)
