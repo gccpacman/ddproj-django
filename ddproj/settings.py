5034f5ba85fb7cc2e8610a8e04214200a2ab7f9f
gccpacman <gccpacman@gmail.com>
2019-11-09 15:27:36 +0800

    remove mysql jsonfield

--- ddproj/settings.py
+++ ddproj/settings.py
@@ -80,33 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ddproj.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ddproj-dev',
-        'USER': 'ddxyz',
-        'PASSWORD': 'dDxYz1@3#e',
-        'HOST': 'kr1.cakrcyzdnyyv.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'connect_timeout': 5
-        },
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -143,57 +116,94 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/backend/static/'
+# STATIC_URL = '/backend/static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "ddfront/dist/static"),
 # ]
+       
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ddproj-dev',
+#         'USER': 'ddxyz',
+#         'PASSWORD': 'dDxYz1@3#e',
+#         'HOST': 'kr1.cakrcyzdnyyv.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#             'connect_timeout': 5
+#         },
+#     }
+# }
 
 SHANGHAI_LIBRARY_API_KEY = '3f5df65840c93fea3c1026b8a64649dffa3d6328'
 BAIDU_MAP_JSAPI_KEY = 'bRrHftKV7wBPHYFSkp2GRZQCVGbz8nhy'
 GAODE_MAP_WEBAPI_KEY = '82455bd226d65b5c36768ad1cd410fea'
 
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8080",
+    "http://localhost:8080",
+    "http://dev.datadude.xyz:8080"
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-if socket.gethostname() == 'ip-172-31-44-132':
-    DEBUG = False
-    ALLOWED_HOSTS = [
-        "api.datadude.xyz",
-        "api2.datadude.xyz"
-    ]
-    CORS_ORIGIN_WHITELIST = [
-        "http://datadude.xyz",
-        "https://datadude.xyz",
-        "http://www.datadude.xyz",
-        "https://www.datadude.xyz",
-        "http://shlib.datadude.xyz",
-        "https://shlib.datadude.xyz"
-    ]
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ddproj',
-            'USER': 'ddxyz',
-            'PASSWORD': 'dDxYz1@3#e',
-            'HOST': 'kr1.cakrcyzdnyyv.ap-northeast-2.rds.amazonaws.com',
-            'PORT': '3306',
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-                'connect_timeout': 5
-            },
-        }
-    }
-    STATIC_ROOT = '/home/ubuntu/collected_static/'
-    MEDIA_ROOT = '/home/ubuntu/media/'
-    MEDIA_URL = '/backend/media/'
-else:
-    ALLOWED_HOSTS = ["*"]
-    CORS_ORIGIN_WHITELIST = [
-        "http://127.0.0.1:8080",
-        "http://localhost:8080",
-        "http://dev.datadude.xyz:8080"
-    ]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID='AKIA564S4C36TY524PYP'
+AWS_SECRET_ACCESS_KEY='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'
+AWS_STORAGE_BUCKET_NAME='staticfiles.datadude.xyz'
+AWS_LOCATION='shlibdev/'
+AWS_S3_CUSTOM_DOMAIN = 'staticfiles.datadude.xyz'
+STATIC_URL='/shlibdev/'
+
+
+# if socket.gethostname() == 'ip-172-31-44-132':
+#     DEBUG = False
+#     ALLOWED_HOSTS = [
+#         "api.datadude.xyz",
+#         "api2.datadude.xyz"
+#     ]
+#     CORS_ORIGIN_WHITELIST = [
+#         "http://datadude.xyz",
+#         "https://datadude.xyz",
+#         "http://www.datadude.xyz",
+#         "https://www.datadude.xyz",
+#         "http://shlib.datadude.xyz",
+#         "https://shlib.datadude.xyz"
+#     ]
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'ddproj',
+#             'USER': 'ddxyz',
+#             'PASSWORD': 'dDxYz1@3#e',
+#             'HOST': 'kr1.cakrcyzdnyyv.ap-northeast-2.rds.amazonaws.com',
+#             'PORT': '3306',
+#             'OPTIONS': {
+#                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#                 'charset': 'utf8mb4',
+#                 'connect_timeout': 5
+#             },
+#         }
+#     }
+#     STATIC_ROOT = '/home/ubuntu/collected_static/'
+#     MEDIA_ROOT = '/home/ubuntu/media/'
+#     MEDIA_URL = '/backend/media/'
+# else:
+
+ 
