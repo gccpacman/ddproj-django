c50d82fba22e3bafa6b4906aa6eb3e3e8bfb9e9d
gccpacman <gccpacman@gmail.com>
2020-05-18 15:12:01 +0800

    update db info

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.7-alpine
 
 RUN sed -i "s/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g" /etc/apk/repositories 
-RUN apk add --update mysql-dev build-base jpeg-dev zlib-dev libjpeg
+RUN apk add --update mysql-dev build-base jpeg-dev zlib-dev libjpeg curl mysql-client
 
 WORKDIR /app
 
--- ddproj/settings.py
+++ ddproj/settings.py
@@ -83,26 +83,10 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ddproj',
-        'USER': 'ddxyz',
-        'PASSWORD': 'dDxYz1@3#e',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'connect_timeout': 5
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
