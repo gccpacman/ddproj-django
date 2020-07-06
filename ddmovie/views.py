645c908770d4ce53e58ec6746979b08f476b251c
gccpacman <gccpacman@gmail.com>
2020-07-06 10:09:21 +0800

    update movie view

--- ddmovie/views.py
+++ ddmovie/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import serializers, generics, filters
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ddmovie.models import Movie, MoviePeople, MoviePhoto, MoviePhotoPeople, MovieCinema
 
-# Create your views here.
+class MovieCinemaSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = MovieCinema
+        fields = [
+            '_id',
+            'name',
+            'nameEn',
+            'architectureUri',
+        ]
