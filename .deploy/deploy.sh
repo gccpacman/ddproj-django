f6bb42eab1eaf5daef17bdae84f4374286eb6b82
gccpacman <gccpacman@gmail.com>
2020-07-08 10:11:49 +0800

    Update deploy.sh

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 echo "1. Copy source code to host:"
 scp -r ../* tc1:/root/ddproj-django/
 
-echo "2. Start docker compose:"
-ssh tc1 "cd /root/ddproj-django/.deploy/prod; docker-compose up -d"
+echo "2. build docker image & start docker compose"
+ssh tc1 "cd /root/ddproj-django/.deploy/prod; docker-compose build; docker-compose up -d;"
