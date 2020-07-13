5f0984d28c898bdc34599c3eaa71bf931a4149ef
gccpacman <gccpacman@gmail.com>
2020-07-13 17:24:22 +0800

    update deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,7 +3,8 @@
 cd "$(dirname "$0")"
 
 echo "1. Copy source code to host:"
-scp -r ../* tc1:/root/ddproj-django/
+rsync -avz --progress --exclude '.deploy' --exclude '.git' --exclude '*.pyc' --exclude '**/__pycache__/' ../* tc1:/root/ddproj-django/
+
 
 echo "2. build docker image & start docker compose"
 ssh tc1 "cd /root/ddproj-django/.deploy/prod; docker-compose build; docker-compose up -d;"
