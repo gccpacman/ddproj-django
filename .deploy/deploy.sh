06c7c39d3fadc118bb9389ddf8285dff784108f2
gccpacman <gccpacman@gmail.com>
2020-07-14 23:31:33 +0800

    fix

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -5,6 +5,5 @@ cd "$(dirname "$0")"
 echo "1. Copy source code to host:"
 rsync -avz --progress --exclude '.deploy' --exclude '.git' --exclude '*.pyc' --exclude '**/__pycache__/' ../* tc1:/root/ddproj-django/
 
-
-echo "2. build docker image & start docker compose"
-ssh tc1 "cd /root/ddproj-django/.deploy/prod; docker-compose build; docker-compose up -d;"
+echo "2. docker build"
+ssh tc1 "cd /root/ddproj-django/.deploy/prod; docker-compose build; docker-compose up -d"
