624775aef6ca8353e9f5b54b3de3b964031634e9
gccpacman <gccpacman@gmail.com>
2020-07-21 15:34:16 +0800

    custom paginatin

--- ddproj/settings.py
+++ ddproj/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'ddproj.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'ddproj.pagination.RestfulPagination',
     'DEFAULT_FILTER_BACKENDS':
     ['django_filters.rest_framework.DjangoFilterBackend'],
     'PAGE_SIZE': 20
